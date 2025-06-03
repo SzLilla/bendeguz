62fbf4b5a3904b8f1d5e0ce15987dd2da92ed363
SzLilla <szabolcsililla@gmail.com>
2025-06-03 23:39:27 +0200

    add some cards

--- src/index.php
+++ src/index.php
@@ -7,9 +7,67 @@
 
 <body>
     <?php include '../src/header.php'; ?>
-    <?php include '../src/footer.php'; ?>
-    content<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    content
+    <!-- <?php include '../src/footer.php'; ?> TODO not fix -->
+
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-8 p-3 text-white">
+                <div class="row">
+                    <div class="col p-3 text-white">
+                        <div class="card">
+                            <img class="card-img-top" src="../public/images/cardimages/uszopelenka.jpg"
+                                alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Úszópelenkák</h4>
+                                <p class="card-text">Some example text.</p>
+                                <a href="#" class="btn btn-primary">Tovább</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col p-3 text-white">
+                        <div class="card">
+                            <img class="card-img-top" src="../public/images/cardimages/uszopelenka.jpg"
+                                alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Úszópelenkák</h4>
+                                <p class="card-text">Some example text.</p>
+                                <a href="#" class="btn btn-primary">Tovább</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col p-3 text-white">
+                        <div class="card">
+                            <img class="card-img-top" src="../public/images/cardimages/uszopelenka.jpg"
+                                alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Úszópelenkák</h4>
+                                <p class="card-text">Some example text.</p>
+                                <a href="#" class="btn btn-primary">Tovább</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col p-3 text-white">
+                        <div class="card">
+                            <img class="card-img-top" src="../public/images/cardimages/uszopelenka.jpg"
+                                alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Úszópelenkák</h4>
+                                <p class="card-text">Some example text.</p>
+                                <a href="#" class="btn btn-primary">Tovább</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col p-3 bg-primary text-white">col1</div>
+                    <div class="col p-3 bg-primary text-white">col2</div>
+                    <div class="col p-3 bg-primary text-white">col3</div>
+                    <div class="col p-3 bg-primary text-white">col4</div>
+                </div>
+            </div>
+            <div class="col-4 p-3 bg-dark text-white">.col</div>
+        </div>
+    </div>
 </body>
 
 </html>
