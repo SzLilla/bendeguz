467ae961dd46b795161d45cd9239e2993c7ba177
SzLilla <szabolcsililla@gmail.com>
2025-06-08 08:35:15 +0200

    moving to folder

--- public_html/commonheader.php
+++ public_html/commonheader.php
@@ -5,5 +5,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php
-define("BASE_URL", "http://localhost"); // "https://www.bendeguz.org"); //TODO
+// define("BASE_URL", "http://localhost"); //TODO
+define("BASE_URL", "https://www.bendeguz.org"); //TODO
 ?> 
