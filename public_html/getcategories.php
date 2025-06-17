2eea96847c168022c94a80c63032689298c62ae1
SzLilla <szabolcsililla@gmail.com>
2025-06-17 13:01:02 +0200

    default card images

--- public_html/getcategories.php
+++ public_html/getcategories.php
@@ -1,5 +1,5 @@
 <?php
-$sql = "SELECT id, name, img_url FROM categories";
+$sql = "SELECT id, name, IFNULL(NULLIF(img_url, ''), 'public/images/cardimages/default.jpg') AS img_url FROM categories";
 $result = $conn->query($sql);
 $conn->close();
 
