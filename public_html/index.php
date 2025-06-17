54e0e1aa68b5d3ad3dad349222f4ec9e3990a747
SzLilla <szabolcsililla@gmail.com>
2025-06-17 23:30:26 +0200

    - add favicon - add category link with get - add 'hamarosan'

--- public_html/index.php
+++ public_html/index.php
@@ -15,6 +15,20 @@
             <div class="row">
                 <div class="col-8">
                     <div class="row">
+                        <div class="col-12 mb-4">
+                            <div class="card">
+                                <img src="/public/images/hamarosan.jpg" class="card-img-top" alt="Hamarosan"
+                                    style="height: 300px; object-fit: cover;">
+                                <div class="card-img-overlay text-center text-white"  style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);">
+                                    <h1 class="card-title">Hamarosan</h1>
+                                    <div class="card-body">
+                                        <p class="card-text">Új kategóriák érkeznek!</p>
+                                    </div>;
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
                         <?php
                         if ($result->num_rows > 0) {
                             while ($row = $result->fetch_assoc()) {
@@ -23,7 +37,7 @@
                                 echo '    <img src="' . htmlspecialchars($row["img_url"]) . '" class="card-img-top" alt="' . htmlspecialchars($row["name"]) . '">';
                                 echo '    <div class="card-body">';
                                 echo '      <h5 class="card-title">' . htmlspecialchars($row["name"]) . '</h5>';
-                                echo '      <a href="#" class="btn btn-primary">Tovább</a>';
+                                echo '      <a href="category.php?id=' . htmlspecialchars($row["id"]) . '" class="btn btn-primary">Tovább</a>';
                                 echo '    </div>';
                                 echo '  </div>';
                                 echo '</div>';
@@ -34,7 +48,7 @@
                         ?>
                     </div>
                 </div>
-                <div class="col-4 p-3 bg-dark text-white">...</div>
+                <div class="col-4 p-3 bg-light"></div>
             </div>
         </div>
 
